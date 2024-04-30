35e371ba0f5b8a0c70b377fb75f18e81af5429c8
AlexSearha <contact@mytechcompanion.fr>
2024-05-01 00:37:56 +0200

    folder page search bar placement

--- app/(without-layout)/dashboard/dossiers/page.tsx
+++ app/(without-layout)/dashboard/dossiers/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { lusitana } from '@/app/ui/fonts';
-import { PlusIcon } from '@heroicons/react/20/solid';
-import Search from 'antd/es/transfer/search';
+import { MagnifyingGlassIcon, PlusIcon } from '@heroicons/react/20/solid';
 import { useState } from 'react';
 
 export default function DossiersPage({ params }: { params: { slug: string } }) {
@@ -17,8 +16,19 @@ export default function DossiersPage({ params }: { params: { slug: string } }) {
       >
         Dossiers Page
       </h1>
-      <div id="folder-container" className="h-full mt-10">
+      <div id="folder-container" className="h-full mt-6">
         <div id="folder-crudBar" className="flex justify-between w-full">
+          <button
+            onClick={() => handleAddNewFolder()}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            className="flex justify-end items-center border-2 border-greena-500 text-greena-500 p-2 rounded transition-colors hover:bg-greena-500"
+          >
+            <PlusIcon className={`h-7 w-7 ${isHovered ? 'text-white' : ''}`} />
+            <p className={`font-bold ${isHovered ? 'text-white' : ''}`}>
+              Ajouter
+            </p>
+          </button>
           <input
             type="text"
             placeholder="Recherche"
@@ -29,17 +39,6 @@ export default function DossiersPage({ params }: { params: { slug: string } }) {
             name="search"
             className="focus:ring-greena-400 focus:border-greena-400 rounded"
           />
-          <button
-            onClick={() => handleAddNewFolder()}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-            className="flex justify-end items-center border-2 border-black p-2 rounded transition-colors hover:bg-greena-500"
-          >
-            <PlusIcon className={`h-7 w-7 ${isHovered ? 'text-white' : ''}`} />
-            <p className={`font-bold ${isHovered ? 'text-white' : ''}`}>
-              Ajouter
-            </p>
-          </button>
         </div>
         <div className="flex justify-center mt-4">
           <table id="client-table" className="w-full">
@@ -47,14 +46,18 @@ export default function DossiersPage({ params }: { params: { slug: string } }) {
               <tr className="text-greena-400 text-xl">
                 <th scope="col">ID</th>
                 <th scope="col">Client</th>
-                <th scope="col">Dossier</th>
+                <th scope="col">Protocole</th>
+                <th scope="col">Action</th>
               </tr>
             </thead>
             <tbody className="mt-2">
               <tr className="transition-all cursor-pointer hover:bg-slate-200 text-center hover:ring-1 hover:ring-slate-300">
                 <td>1</td>
                 <td>Alexis Marouf</td>
-                <td>Peur des couilles</td>
+                <td>Peur du vide</td>
+                <td className="flex justify-center gap-2">
+                  <MagnifyingGlassIcon className="h-5 w-5 transition-colors hover:text-greena-400" />
+                </td>
               </tr>
             </tbody>
           </table>
