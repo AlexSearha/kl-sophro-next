a795e5b6496b2037a42e45bf221db7749d0ab9f4
AlexSearha <contact.mytechcompanion.fr>
2024-08-11 18:47:05 +0200

    add datepicker with time selection in appointments userpage

--- app/lib/providers/antThemeProvider.tsx
+++ app/lib/providers/antThemeProvider.tsx
@@ -1,9 +1,11 @@
 import { ConfigProvider } from 'antd';
 import { TailwindTheme } from '@/tailwind.config';
+import frFR from 'antd/locale/fr_FR';
 
 export default function AntThemeProvider({ children }: { children: React.ReactNode }) {
   return (
     <ConfigProvider
+      locale={frFR}
       theme={{
         token: {
           colorPrimary: TailwindTheme.extend.colors.greena[400],
--- app/ui/forms/adminCalendarForm/adminCalendarForm.tsx
+++ app/ui/forms/adminCalendarForm/adminCalendarForm.tsx
@@ -1,72 +1,48 @@
-import { useEffect, useRef, useState } from 'react';
-import { AlertErrorNotification } from '../../alerte-notification';
-import CalendarElement from '../CalendarElement';
-import LoadingSubmitForm from '../../contact/Loading';
-import SelectElement from './elements/SelectElement';
-import InputElement from './elements/InputElement';
-import { useModal } from '@/app/lib/providers/modalProvider';
-import ValidationModal from '../../modals/ValidationModal';
-import { dateIsoToString } from '@/app/lib/date-format';
-import { useLazyPostFetchData } from '@/app/lib/hooks/fetching-hooks';
+import React from 'react';
+import { DatePicker, Space } from 'antd';
+import type { GetProps } from 'antd';
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
 
-export default function AdminCalendarForm() {
-  const { fetchData, isLoading, isError, data } = useLazyPostFetchData();
-  const { dispatch } = useModal();
+type RangePickerProps = GetProps<typeof DatePicker.RangePicker>;
 
-  const [errorMessage, setErrorMessage] = useState('');
-  const [dateValue, setDateValue] = useState<Date | null | undefined>();
-  const [selectValue, setSelectValue] = useState('');
-  const [inputValue, setInputValue] = useState('');
-  const formRef = useRef(null);
+dayjs.extend(customParseFormat);
 
-  async function postAppointment(formData: FormData) {
-    setErrorMessage('');
-    const rowFormData = {
-      date: dateValue?.toISOString().slice(0, 19).replace('T', ' '),
-      hour: selectValue,
-      commentary: inputValue,
-    };
-
-    if (rowFormData.date && rowFormData.hour) {
-      console.log(JSON.stringify(rowFormData));
-      const formatDate = dateIsoToString(rowFormData.date);
-      dispatch({
-        type: 'update_modal',
-        payload: {
-          title: 'Validation',
-          content: (
-            <ValidationModal
-              subTitle={`Validez vous le rendez vous du ${formatDate.date} à ${rowFormData.hour} ? `}
-            />
-          ),
-        },
-      });
-    } else {
-      setErrorMessage('Veuillez selectionner une date et une heure de rendez-vous');
-    }
+const range = (start: number, end: number) => {
+  const result = [];
+  for (let i = start; i < end; i++) {
+    result.push(i);
   }
+  return result;
+};
 
-  useEffect(() => {
-    if (data && formRef.current != null) {
-      (formRef.current as HTMLFormElement).reset();
-    }
-  }, [data, isError]);
+// eslint-disable-next-line arrow-body-style
+const disabledDate: RangePickerProps['disabledDate'] = (current) => {
+  // Can not select days before today and today
+  return current && current < dayjs().endOf('day');
+};
 
-  return (
-    <>
-      <form ref={formRef} action={postAppointment} className="flex flex-col justify-center gap-2">
-        <CalendarElement setDateValue={setDateValue} />
-        <SelectElement setSelectValue={setSelectValue} />
-        <InputElement setInputValue={setInputValue} />
-        <button
-          type="submit"
-          className="px-4 py-2 mb-2 bg-greena-400 text-white font-semibold rounded transition-all hover:bg-greena-500 focus:outline-none focus:ring focus:border-greena-400"
-        >
-          {isLoading ? <LoadingSubmitForm /> : 'Réserver'}
-        </button>
-        {isError ? <AlertErrorNotification message="Une erreur s'est produite" /> : null}
-      </form>
-      {errorMessage.length > 0 ? <AlertErrorNotification message={errorMessage} /> : null}
-    </>
-  );
-}
+const disabledDateTime = () => ({
+  disabledHours: () => {
+    const hours = range(0, 24);
+    hours.splice(9, 10);
+    return hours;
+  },
+  // disabledMinutes: () => range(30, 60),
+});
+
+const AdminCalendarForm: React.FC = () => (
+  <Space direction="vertical" size={12}>
+    <DatePicker
+      format="le ddd DD,MM YYYY à HH:mm"
+      className="w-[350px]"
+      size="large"
+      disabledDate={disabledDate}
+      disabledTime={disabledDateTime}
+      minuteStep={30}
+      showTime={{ defaultValue: dayjs('00:00:00', 'HH:mm') }}
+    />
+  </Space>
+);
+
+export default AdminCalendarForm;
